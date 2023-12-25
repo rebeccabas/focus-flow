e1c89a7a38eb3d5b78e4cc8885456e43738813cb
Rubika Bashyal <bashyalrubika@gmail.com>
2023-12-26 01:10:10 +0545

    login page fixes

--- frontend/src/pages/login-signup.jsx
+++ frontend/src/pages/login-signup.jsx
@@ -1,18 +1,20 @@
-// LoginPage.jsx
 import React, { useState } from 'react';
 import './loginpage.css';
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 function LoginForm() {
     return (
         <>
-            <form class="space-y-4 mr-50 justify-center items-center" action="#">
+            <form class="space-y-4 justify-center items-center" action="#">
                 <div className='justify-center items-center'>
                     <div>
-                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                        <label for="email" class="flex mb-2 text-sm font-medium text-gray-900 dark:text-white"> <FaUser className='mt-1 mr-1' />Username</label>
                         <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                     </div>
                     <div>
-                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <label for="password" class="flex mt-3 mb-2 text-sm font-medium text-gray-900 dark:text-white">< FaLock className='mt-1 mr-1' />Password</label>
                         <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                     </div>
                     <div class="flex items-center justify-between">
@@ -39,29 +41,32 @@ function LoginForm() {
 function SignUp() {
     return (
         <>
-            <form class="space-y-4 flex-col ml-10 " action="#">
-                <div class="fex justify-center items-center">
+            <form class="space-y-4  justify-center items-center" action="#">
+                <div class='justify-center items-center'>
                 <div>
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name</label>
+                    <label for="email" class="flex mb-1 text-sm font-medium text-gray-900 dark:text-white"><MdEmail className='mt-1 mr-1' />Email</label>
                     <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                 </div>
                 <div>
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                    <label for="email" class="flex mt-2 mb-1 text-sm font-medium text-gray-900 dark:text-white"> <FaUser className='mt-1 mr-1' />Username</label>
                     <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                 </div>
                 <div>
-                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                    <label for="password" class="flex mt-2 mb-1 text-sm font-medium text-gray-900 dark:text-white">< FaLock className='mt-1 mr-1' />Password</label>
                     <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                 </div>
                 <div>
-                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                    <label for="password" class="flex mt-2 mb-1 text-sm font-medium text-gray-900 dark:text-white">< FaLock className='mt-1 mr-1' />Confirm Password</label>
                     <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                 </div>
                 <div class="flex items-center justify-between">
                     <div class="flex items-start">
-                        <div class="flex items-center h-5">
+                        <div class="flex items-center h-5 pt-2.5">
                             <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
                         </div>
+                        <div class="ml-3 text-sm pt-2">
+                                <label for="remember" class="text-gray-500 dark:text-gray-300">I agree with the terms and conditions.</label>
+                            </div>
                     </div>
                 </div>
                 <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
