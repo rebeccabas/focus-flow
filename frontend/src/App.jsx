fcd770402c90124330d55062e7c0b96696b739ba
Rubika Bashyal <bashyalrubika@gmail.com>
2023-12-29 21:10:09 +0545

    timer added/css rem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/landing_page';
 import LoginPage from './pages/login-signup';
 import HomePage from './pages/sidebar';
+import PomodoroTimer from './pages/timer';
 // import Timer from './pages/timer';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
     <Routes>
       <Route path='/' element={<LandingPage/>} />
       <Route path='/login' element={<LoginPage/>} />
-      {/* <Route path='/timer' element={<Timer/>}/> */}
+      <Route path='/timer' element={<PomodoroTimer/>}/>
       <Route path='/home' element={<HomePage/>}/>
     </Routes>
   )
--- frontend/src/pages/timer.jsx
+++ frontend/src/pages/timer.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-function timer() {
+const PomodoroTimer = () => {
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        if (seconds === 0) {
+          if (minutes === 0) {
+            clearInterval(interval);
+            setIsActive(false);
+            setMinutes(25);
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        } else {
+          setSeconds(seconds - 1);
+        }
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, minutes, seconds]);
+
+  const toggleTimer = () => {
+    setIsActive(!isActive);
+  };
+
+  const resetTimer = () => {
+    setIsActive(false);
+    setMinutes(25);
+    setSeconds(0);
+  };
+
   return (
-    <>
-    </>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-3xl mb-4">{String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}</div>
+      <div className="space-x-4">
+        <button
+          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+          onClick={toggleTimer}
+        >
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+          onClick={resetTimer}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default timer
+export default PomodoroTimer;
